fd7db9e15885bbdf576a4b2b81513da0d1003ee0
EusaHack <eusa510@gmail.com>
2024-12-04 08:53:39 -0600

    [UPDATE] vista login

--- project/index/src/components/Login/LoginRender.jsx
+++ project/index/src/components/Login/LoginRender.jsx
@@ -1,5 +1,23 @@
+import "./LoginRender.css"
 export  function LoginRender() {
   return (
-    <div>LoginRender</div>
+    <div className="container-log">
+        <div className="imageSection-log">
+          <img src="images/login-animation.gif"
+            className="image-log"
+            alt="Imagen de animación"
+          />
+        </div>
+        <div className="formSection-log">
+          <h1 className="title-log">¡HOLA!</h1>
+          <p className="txt-log"> Por favor llena los siguientes campos  </p>
+          <p className="txt-log">Correo electronico</p>
+          <input className="input-log" type="email" placeholder="Ingresa tu correo"></input>
+          <p className="txt-log">Contraseña</p>
+          <input className="input-log" type="password" placeholder="Ingresa tu contraseña"></input>
+          <button className="btn-log">INGRESAR</button>
+          <a className="vin-log" href="">Olvide contraseña</a>
+        </div>
+    </div>
   )
 }
