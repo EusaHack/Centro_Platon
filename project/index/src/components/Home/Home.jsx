3c449b36126d82532696a50f088e11161357e027
EusaHack <eusa510@gmail.com>
2024-12-04 01:20:43 -0600

    [UPDATE]  pantalla nuestros servicios

--- project/index/src/components/Home/Home.jsx
+++ project/index/src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { SideNav } from "../NavBar/SideNav"
 import { HomeRender } from "./HomeRender"
 import { MenuRender } from "./MenuRender"
+import { ServiciosRender } from "./ServiciosRender"
 export function Home() {
   return (
     <>
         <SideNav />
         <HomeRender />
         <MenuRender />
+        <ServiciosRender />
     </>
   )
 }
